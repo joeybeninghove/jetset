7b3a268681916e7300a27f778042f8a0bcca8ae8
Joey Beninghove <me@joey.io>
2019-01-06 23:24:07 -0500

    Replace postcss-next with postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss')('./tailwind.js'),
-    require('postcss-cssnext')
+    require('postcss-preset-env')
   ]
 }
